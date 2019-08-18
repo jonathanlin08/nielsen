1609f381b11412b4b1458e481b602d74b3ece762
jlin <jonathanlin08@gmail.com>
2019-08-18 14:50:07 -0400

    Move data.sql to test resources

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO
-	APPOINTMENTS (customer_name, appointment_date, status, price)
-VALUES
-  	('Jon', '2019-08-01 08:00', 'PENDING', 450.50),
-  	('Eric', '2019-08-01 10:00', 'PROCESSING', 725.50),
-  	('Mike', '2019-08-01 12:00', 'COMPLETE', 1000.00);
